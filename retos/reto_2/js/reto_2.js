3e2dabddd36748593e56105bcf9c56d1b0018329
DiegoC01 <diego_canquil@outlook.es>
2023-09-15 13:06:42 -0300

    Arreglando algunas cosas en el reto 2

--- retos/reto_2/js/reto_2.js
+++ retos/reto_2/js/reto_2.js
@@ -1,15 +1,24 @@
 alert("Este pequeñito programa calculará el promedio de 3 notas");
 
-let nota1 = prompt("Ingresa tu primera nota:", '');
-let nota2 = prompt("Ingresa tu segunda nota:", '');
-let nota3 = prompt("Ingresa tu tercera nota:", '');
+let nota1 = prompt("Ingresa tu primera nota:", '') * 0.4;
+let nota2 = prompt("Ingresa tu segunda nota:", '') * 0.3;
+let nota3 = prompt("Ingresa tu tercera nota:", '') * 0.3;
+
+if (false) {
+    alert("Haz ingresado un caracter no valido para calcular tu promedio.");
+}
+else {
+    promedio_ponderado = nota1 + nota2 + nota3;
+
+    alert(`Tu nota es ${promedio_ponderado}`);
     
-
-nota1 = Number(nota1) * 0.4;
-nota2 = Number(nota2) * 0.3;
-nota3 = Number(nota3) * 0.3;
-
-promedio_ponderado = nota1 + nota2 + nota3;
-
-alert(`Tu nota es ${promedio_ponderado}`);
-
+    if (promedio_ponderado < 3.95) {
+        alert("Has reprobado la asignatura :(");
+    } 
+    else if (promedio_ponderado >= 3.95 && promedio_ponderado <= 4.94) {
+        alert("Vas a examen :|");
+    }
+    else if (promedio_ponderado >= 4.95) {
+        alert("Eximiste la asignatura :)!")
+    }    
+}
