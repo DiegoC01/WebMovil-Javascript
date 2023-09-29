a6b097bda09861f6749ec95a8452fd08d5a46866
DiegoC01 <diego_canquil@outlook.es>
2023-09-29 12:48:47 -0300

    Arreglando algunas cositas en el reto 3

--- retos/reto_3/js/reto_3.js
+++ retos/reto_3/js/reto_3.js
@@ -1,11 +1,13 @@
 function calcularPropina(){
+    // Obteniendo valores ingresados por el usuario
     let total_boleta = document.getElementById("total_boleta").value;
     let porcentaje_propina = document.getElementById("porcentaje_propina").value / 100;
 
+    // Calculando propina
     let propina = total_boleta * porcentaje_propina;
 
+    // Mostrando propina y total final boleta en la página
     document.getElementById("mostrar_propina").innerHTML = `Propina: $${propina} CLP`;
-
     let total_boleta_final = Number(propina) + Number(total_boleta);
     document.getElementById("mostrar_total_boleta_final").innerHTML = `Total a pagar: $${total_boleta_final} CLP`;
 
