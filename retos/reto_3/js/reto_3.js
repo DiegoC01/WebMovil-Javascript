234e4dee9d5508951ac3c2446cf5a7bb6d18ca03
DiegoC01 <diego_canquil@outlook.es>
2023-10-01 00:00:48 -0300

    Finalizando reto 3

--- retos/reto_3/js/reto_3.js
+++ retos/reto_3/js/reto_3.js
@@ -3,12 +3,21 @@ function calcularPropina(){
     let total_boleta = document.getElementById("total_boleta").value;
     let porcentaje_propina = document.getElementById("porcentaje_propina").value / 100;
 
-    // Calculando propina
-    let propina = total_boleta * porcentaje_propina;
+    // Validación: Si el total de la boleta es positivo y no es decimal.
+    let condicion_1 = (total_boleta % 1 == 0);
+    let condición_2 = (total_boleta > 0);
 
-    // Mostrando propina y total final boleta en la página
-    document.getElementById("mostrar_propina").innerHTML = `Propina: $${propina.toFixed(2)} CLP`;
-    let total_boleta_final = Number(propina) + Number(total_boleta);
-    document.getElementById("mostrar_total_boleta_final").innerHTML = `Total a pagar: $${total_boleta_final.toFixed(2)} CLP`;
+    if(condicion_1 && condición_2){
+        // Calculando propina
+        let propina = total_boleta * porcentaje_propina;
+
+        // Mostrando propina y total final boleta en la página
+        document.getElementById("mostrar_propina").innerHTML = `Propina: $${propina.toFixed(2)} CLP`;
+        let total_boleta_final = Number(propina) + Number(total_boleta);
+        document.getElementById("mostrar_total_boleta_final").innerHTML = `Total a pagar: $${total_boleta_final.toFixed(2)} CLP`;
+    }
+    else{
+        alert("Se ha ingresado un valor no válido!");
+    }
 
 }
